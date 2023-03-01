99e9e9281cd07d6a423fa31ce671c007bed1f564
Asyamdaff <sasyam011@gmail.com>
2023-03-01 11:27:20 +0700

    first commit

---
+++ be-eventList/app/api/v1/categories/controller.js
@@ -0,0 +1,81 @@
+// import model category
+const {
+  getAllCategories,
+  getOneCategories,
+  updateCategories,
+  createCategories,
+  deleteCategories,
+} = require("../../../services/mongoose/categories");
+
+const { StatusCodes } = require("http-status-codes");
+
+// buat function create
+const create = async (req, res) => {
+  try {
+    const result = await createCategories(req);
+
+    // berikan response kepada client dengan mengembalikan product yang baru dibuat
+    res.status(StatusCodes.CREATED).json({
+      data: result,
+    });
+  } catch (err) {
+    // jika terjadi kesalahan kemudian gunakan method `next` agar Express memproses error tersebut
+    next(err);
+  }
+};
+
+const index = async (req, res) => {
+  try {
+    const result = await getAllCategories(req);
+    res.status(StatusCodes.OK).json({
+      data: result,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const find = async (req, res) => {
+  try {
+    const result = await getOneCategories(req);
+
+    res.status(StatusCodes.OK).json({
+      data: result,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const result = await updateCategories(req);
+
+    res.status(StatusCodes.OK).json({
+      data: result,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const destroy = async (req, res) => {
+  try {
+    const result = await deleteCategories(req);
+
+    res.status(StatusCodes.OK).json({
+      data: result,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Export fungsi create pada controller categories
+module.exports = {
+  create,
+  index,
+  find,
+  update,
+  destroy,
+};
