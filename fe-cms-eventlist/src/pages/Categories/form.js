23b13674d545d40da209403ae3b49c7935b86bae
S A M <107426421+ForMeeee@users.noreply.github.com>
2023-03-08 09:35:07 +0700

    Update

--- fe-cms-eventlist/src/pages/Categories/form.js
+++ fe-cms-eventlist/src/pages/Categories/form.js
@@ -1,10 +1,9 @@
-import React from "react";
-import { Figure, Form } from "react-bootstrap";
-import Button from "../../components/Button";
-import TextInputWithLabel from "../../components/TextInputWithLabel";
-import { config } from "../../configs";
+import React from 'react';
+import { Form } from 'react-bootstrap';
+import SButton from '../../components/Button';
+import TextInputWithLabel from '../../components/TextInputWithLabel';
 
-export default function SpeakersForm({
+function CategoriesFormPage({
   handleSubmit,
   form,
   handleChange,
@@ -14,51 +13,23 @@ export default function SpeakersForm({
   return (
     <Form>
       <TextInputWithLabel
-        placeholder={"Masukan nama pembicara"}
-        label={"Nama"}
-        name="name"
+        placeholder={'Masukan nama kategori'}
+        label={'Nama kategori'}
+        name='name'
         value={form.name}
-        type="text"
+        type='text'
         onChange={handleChange}
       />
-      <TextInputWithLabel
-        placeholder={"Masukan role"}
-        label={"Role"}
-        name="role"
-        value={form.role}
-        type="text"
-        onChange={handleChange}
-      />
-      <TextInputWithLabel
-        placeholder={"Masukan Avatar"}
-        label={"Avatar"}
-        name="avatar"
-        // value={form.avatar}
-        type="file"
-        onChange={handleChange}
-      />
-      {form.avatar !== "" && (
-        <div>
-          <Figure>
-            <Figure.Image
-              width={171}
-              height={180}
-              alt="171x180"
-              src={`${config.api_image}/${form.avatar}`}
-            />
-            
-            <Figure.Caption>Perview image avatar</Figure.Caption>
-          </Figure>
-        </div>
-      )}
-      <Button
-        variant="primary"
+      <SButton
+        variant='primary'
         action={handleSubmit}
         loading={isLoading}
         disabled={isLoading}
       >
-        {edit ? "Ubah" : "Simpan"}
-      </Button>
+        {edit ? 'Ubah' : 'Simpan'}
+      </SButton>
     </Form>
   );
 }
+
+export default CategoriesFormPage;
