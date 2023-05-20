47b1c6908ce36ec6406a40fa894b0346c4f0edca
Asyamdaff <sasyam011@gmail.com>
2023-05-20 12:46:05 +0700

    Update .env

--- be-eventList/app/config.js
+++ be-eventList/app/config.js
@@ -2,7 +2,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-  urlDb: process.env.URL_MONGODB_DEV,
+  // urlDb: process.env.URL_MONGODB_DEV,
+  urlDb: process.env.URL_MONGODB,
   jwtSecret: process.env.JWT_SECRET,
   jwtRefreshTokenSecret: process.env.JWT_SECRET_REFRESH_TOKEN,
   jwtExpiration: "72h",
